fc581d6c7f49311fb4e9dbe2895ce3eaf83f641f
Luke Granger-Brown <git@lukegb.com>
2012-08-25 23:09:22 +0100

    Tidy up a little.

--- public/javascripts/js.js
+++ public/javascripts/js.js
@@ -8,10 +8,16 @@ $(function() {
 			eD.removeClass("hidden").find("td div").slideDown("fast");
 			window.location.hash = $this.attr("data-participant");
 		} else {
-			window.location.hash = "";
+			window.location.hash = "_";
 			eD.find("td div").slideUp("fast", function() {
 				$(this).parent().parent().addClass("hidden");
 			});
 		}
 	});
+
+	if (window.location.hash.length > 1) {
+		var h = window.location.hash.substring(1);
+		$(".participant-" + encodeURIComponent(h)).removeClass("hidden").find("td div").show();
+		console.log(h);
+	}
 });
