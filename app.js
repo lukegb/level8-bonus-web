daa3b078488763faa01a1b5dc7ad98f08bd1cc28
Luke Granger-Brown <git@lukegb.com>
2012-08-26 01:02:09 +0100

    Add API for adding scoreboard data.

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.set('secret', process.env.API_SECRET || 'hi');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
@@ -28,7 +29,8 @@ app.configure(function(){
 
     // load up the list of top-20 races
     var db = routes.rounds.db; // seriously? D:
-    db.rounds.find({}, function(err, res) {
+    var c = db.rounds.find().sort({started: -1}).limit(20);
+    c.toArray(function(err, res) {
       if (!err) {
         app.locals.rounds = res;
       } else {
@@ -45,9 +47,19 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
+// -- Index
 app.get('/', routes.index);
 
+// -- Rounds
+//     -- Make new
+app.post('/', routes.rounds.add);
+
+//     -- Fetch
 app.get('/:round_id', routes.rounds.view);
+//     -- Update existing
+app.put('/:round_id', routes.rounds.update);
+//     -- Replace existing
+app.post('/:round_id', routes.rounds.overwrite);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
--- routes/rounds.js
+++ routes/rounds.js
@@ -32,11 +32,69 @@ var fetch_or_404 = function(id, res, callback) {
 exports.view = function(req, res){
   // fetch from DB or 404
   fetch_or_404(req.params.round_id, res, function(round) {
-    
-    res.render('round_view', {
-      round: round,
-      minCapturer: round.participants[0]
-    });
+    if (req.get('Content-Type') == 'application/json') {
+      // just for you :)
+      res.send(200, round);
+    } else {
+      res.render('round_view', {
+        round: round,
+        minCapturer: round.participants[0]
+      });
+    }
+  });
+};
+
+exports.add = function(req, res) {
+  // verify secret
+  if (!req.get('x-secret') || req.get('x-secret') != req.app.get('secret')) {
+    return res.send(403);
+  }
+
+  var storeObj = req.body;
+  storeObj.started = new Date();
+  // done :P
+  db.rounds.insert(storeObj, {safe: true}, function(err, resu) {
+    if (err) {
+      res.send(500, {error: err});
+    }
+    res.set('Location', '/' + resu['_id']).send(201);
+  });
+};
+
+exports.update = function(req, res) {
+  // verify secret
+  if (!req.get('x-secret') || req.get('x-secret') != req.app.get('secret')) {
+    return res.send(403);
+  }
+
+  var storeObj = req.body;
+  db.rounds.update({_id: new mongo.ObjectId(req.params.round_id)}, {"$set": storeObj}, {safe:true}, function(err, resu) {
+    if (err) {
+      res.send(500, {error: err});
+    }
+    if (!resu) {
+      res.send(404);
+    }
+    res.send(204);
+  });
+};
+
+exports.overwrite = function(req, res) {
+  // verify secret
+  if (!req.get('x-secret') || req.get('x-secret') != req.app.get('secret')) {
+    return res.send(403);
+  }
+
+  var storeObj = req.body;
+  storeObj['_id'] = new mongo.ObjectId(req.params.round_id);
+  db.rounds.update({_id: new mongo.ObjectId(req.params.round_id)}, storeObj, {safe:true, upsert:true}, function(err, resu) {
+    if (err) {
+      res.send(500, {error: err});
+    }
+    if (!resu) {
+      res.send(404);
+    }
+    res.send(204);
   });
 };
 
